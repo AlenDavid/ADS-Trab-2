360dc8547c43aa7c405948d2baf5ca265752834c
Samuel <samuelss.blu@gmail.com>
2024-12-02 02:01:19 -0300

    Minor adjustments

--- simulator.R
+++ simulator.R
@@ -16,6 +16,8 @@ simulate_queue <- function(mean_arrival, sd_arrival, mean_service, sd_service, n
   finish_service <- numeric(n_events) 
   waiting_time <- numeric(n_events) 
   active_servers <- rep(0, servers)
+  queue_lengths <- numeric(n_events)
+  server_states <- matrix(0, nrow = n_events, ncol = servers)  # Server states for each event
   
   # Simulation logic
   for (i in 1:n_events) {
@@ -25,23 +27,19 @@ simulate_queue <- function(mean_arrival, sd_arrival, mean_service, sd_service, n
     waiting_time[i] <- start_service[i] - arrivals[i]
     finish_service[i] <- start_service[i] + service_times[i]
     active_servers[free_server] <- finish_service[i]
+    server_states[i, ] <- active_servers  # Log server states
     queue_lengths[i] <- sum(arrivals < start_service[i] & finish_service > arrivals)
   }
   
   total_service_time <- sum(service_times)
   total_time <- max(finish_service) - min(arrivals)
   
-  # 1. Utilization
+  # Metrics
   lambda <- 1 / mean_arrival
   mu <- 1 / mean_service
   U <- lambda / (servers * mu)
-  
   P <- sum(waiting_time > 0) / n_events
-  
-  # 3. Average waiting time in the queue (Wq)
   W_q <- P / (servers * mu * (1 - U))
-  
-  # 4. Average number of calls in the queue (Lq)
   L_q <- lambda * W_q
   
   list(
@@ -51,12 +49,14 @@ simulate_queue <- function(mean_arrival, sd_arrival, mean_service, sd_service, n
       avg_calls_on_queue = L_q
     ),
     events = data.frame(
+      Iteration = 1:n_events,
       Arrival = arrivals,
       StartService = start_service,
       FinishService = finish_service,
       WaitingTime = waiting_time,
       ServiceTime = service_times,
       QueueLength = queue_lengths
-    )
+    ),
+    server_states = server_states
   )
 }
